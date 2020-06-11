19f812467b381d273ddfd5fe5c5196dd7b90e36a
Matt Moncayo <matthew.moncayo@gmail.com>
2020-06-10 23:57:18 -0700

    updated function to write file into html

--- Develop/app.js
+++ Develop/app.js
@@ -4,6 +4,9 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -120,8 +123,9 @@ const subMenu = () => {
 
         case 'No more employees':
 
-            render(employees)
-        // return;
+            render(employees);
+            let results = render(employees)
+            return writeFileAsync(outputPath, results);
 
     }
 }
@@ -195,4 +199,3 @@ const subMenu3 = () => {
 
 
 menu();
-module.exports = Employee;
